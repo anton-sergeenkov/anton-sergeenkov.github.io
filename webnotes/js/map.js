d7a5a9e653badf3c3376bf6302b0670534cf2dc4
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-04-04 17:43:28 +0300

    js

---
+++ webnotes/js/map.js
@@ -0,0 +1,62 @@
+var mapHTML = '';
+
+h1('JavaScript');
+h2('Клиент-сервер');
+div('js', 'client-server-desc',  'Клиент-сервер');
+div('js', 'client-server-http',  'HTTP');
+div('js', 'client-server-https', 'HTTPS');
+div('js', 'client-server-rest',  'REST API');
+
+h2('Client Storage');
+div('js', 'data-storage', 'localStorage / sessionStorage');
+div('js', 'data-db',      'IndexedDB');
+div('js', 'data-cookie',  'Cookie');
+
+h2('Парадигмы программирования');
+div('theory', 'paradigms', 'Парадигмы программирования');
+div('theory', 'paradigms-imper', 'Императивное');
+div('theory', 'paradigms-decl', 'Декларативное');
+div('theory', 'paradigms-func', 'Функциональное');
+div('theory', 'paradigms-oop', 'Объектно-ориентированное');
+div('theory', 'composition', 'Композиция');
+div('theory', 'currying', 'Каррирование / Частичное применение функций');
+
+h2('Методологии разработки');
+div('theory', 'method-dev', 'Методологии разработки');
+div('theory', 'method-waterfall', 'Waterfall');
+div('theory', 'method-agile', 'Agile');
+div('theory', 'method-scrum', 'Scrum');
+div('theory', 'method-kanban', 'Kanban');
+
+h2('');
+div('theory', '', '');
+
+h2('');
+div('theory', '', '');
+
+h2('');
+div('theory', '', '');
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#elements-menu').innerHTML = mapHTML;
+
+function h1(content) {
+	mapHTML += `<h1>${content}</h1>`;
+}
+function h2(content) {
+	mapHTML += `<h2>${content}</h2>`;
+}
+function div(page, theme, content) {
+	mapHTML += `<div class="element-menu" data-page="${page}" data-theme="${theme}">${content}</div>`;
+}
+
+
+
