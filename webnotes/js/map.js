08f300af9a387548d9c7d449040c22fd11c96d58
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-04-05 09:43:03 +0300

    js

--- webnotes/js/map.js
+++ webnotes/js/map.js
@@ -13,26 +13,33 @@ div('js', 'data-db',      'IndexedDB');
 div('js', 'data-cookie',  'Cookie');
 
 h2('Парадигмы программирования');
-div('theory', 'paradigms', 'Парадигмы программирования');
+div('theory', 'paradigms',       'Парадигмы программирования');
 div('theory', 'paradigms-imper', 'Императивное');
-div('theory', 'paradigms-decl', 'Декларативное');
-div('theory', 'paradigms-func', 'Функциональное');
-div('theory', 'paradigms-oop', 'Объектно-ориентированное');
-div('theory', 'composition', 'Композиция');
-div('theory', 'currying', 'Каррирование / Частичное применение функций');
+div('theory', 'paradigms-decl',  'Декларативное');
+div('theory', 'paradigms-func',  'Функциональное');
+div('theory', 'paradigms-oop',   'Объектно-ориентированное');
+div('theory', 'composition',     'Композиция');
+div('theory', 'currying',        'Каррирование / Частичное применение функций');
 
 h2('Методологии разработки');
-div('theory', 'method-dev', 'Методологии разработки');
+div('theory', 'method-dev',       'Методологии разработки');
 div('theory', 'method-waterfall', 'Waterfall');
-div('theory', 'method-agile', 'Agile');
-div('theory', 'method-scrum', 'Scrum');
-div('theory', 'method-kanban', 'Kanban');
+div('theory', 'method-agile',     'Agile');
+div('theory', 'method-scrum',     'Scrum');
+div('theory', 'method-kanban',    'Kanban');
 
 h2('Функции');
-div('js_functions', 'special', 'Особенности');
-div('js_functions', 'announcement', 'Function Declaration / Function Expression / IIFE / new Function');
-div('js_functions', '', '');
-div('js_functions', '', '');
+div('js_functions', 'special',            'Особенности');
+div('js_functions', 'announcement',       'FD / FE / IIFE / new Function');
+div('js_functions', 'arguments',          'Arguments');
+div('js_functions', 'default-parameters', 'Default Parameters (ES6)');
+div('js_functions', 'rest-parameters',    'Rest Parameters (ES6)');
+div('js_functions', 'arrow',              'Стрелочные функции (ES6)');
+div('js_functions', 'closures-theory',    'Замыкания (теория)');
+div('js_functions', 'closures',           'Замыкания');
+div('js_functions', 'recursion',          'Рекурсия');
+div('js_functions', 'named',              'Named Function Expression');
+
 
 h2('');
 div('theory', '', '');
