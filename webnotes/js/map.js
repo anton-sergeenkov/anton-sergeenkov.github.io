e61641baa0592c9b9eecc0b0b76c79f870e5d995
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-04-08 11:06:18 +0300

    js

--- webnotes/js/map.js
+++ webnotes/js/map.js
@@ -1,16 +1,37 @@
-var mapHTML = '';
+var mapHTML1 = '';
+var mapHTML2 = '';
+var currentHTML = 1;
 
+////////////////////////////////////////////////////////////////
+currentHTML = 1;
 h1('JavaScript');
-h2('Клиент-сервер');
-div('js', 'client-server-desc',  'Клиент-сервер');
-div('js', 'client-server-http',  'HTTP');
-div('js', 'client-server-https', 'HTTPS');
-div('js', 'client-server-rest',  'REST API');
 
 h2('Client Storage');
 div('js', 'data-storage', 'localStorage / sessionStorage');
 div('js', 'data-db',      'IndexedDB');
 div('js', 'data-cookie',  'Cookie');
+
+h2('Функции');
+div('js_functions', 'special',            'Особенности');
+div('js_functions', 'announcement',       'FD / FE / IIFE / new Function');
+div('js_functions', 'arguments',          'Arguments');
+div('js_functions', 'default-parameters', 'Default Parameters (ES6)');
+div('js_functions', 'rest-parameters',    'Rest Parameters (ES6)');
+div('js_functions', 'arrow',              'Стрелочные функции (ES6)');
+div('js_functions', 'closures-theory',    'Замыкания (теория)');
+div('js_functions', 'closures',           'Замыкания');
+div('js_functions', 'recursion',          'Рекурсия');
+div('js_functions', 'named',              'Named Function Expression');
+
+////////////////////////////////////////////////////////////////
+currentHTML = 2;
+h1('Theory');
+
+h2('Клиент-сервер');
+div('js', 'client-server-desc',  'Клиент-сервер');
+div('js', 'client-server-http',  'HTTP');
+div('js', 'client-server-https', 'HTTPS');
+div('js', 'client-server-rest',  'REST API');
 
 h2('Парадигмы программирования');
 div('theory', 'paradigms',       'Парадигмы программирования');
@@ -27,17 +48,10 @@ div('theory', 'method-waterfall', 'Waterfall');
 div('theory', 'method-agile',     'Agile / Scrum / Kanban');
 div('theory', 'estimation',       'Эстимация');
 
-h2('Функции');
-div('js_functions', 'special',            'Особенности');
-div('js_functions', 'announcement',       'FD / FE / IIFE / new Function');
-div('js_functions', 'arguments',          'Arguments');
-div('js_functions', 'default-parameters', 'Default Parameters (ES6)');
-div('js_functions', 'rest-parameters',    'Rest Parameters (ES6)');
-div('js_functions', 'arrow',              'Стрелочные функции (ES6)');
-div('js_functions', 'closures-theory',    'Замыкания (теория)');
-div('js_functions', 'closures',           'Замыкания');
-div('js_functions', 'recursion',          'Рекурсия');
-div('js_functions', 'named',              'Named Function Expression');
+h2('Системы контроля версий');
+div('git', 'terminal', 'Git-команды');
+div('git', 'gitflow',  'Gitflow');
+
 
 
 h2('');
@@ -55,16 +69,32 @@ div('theory', '', '');
 
 
 
-document.querySelector('#elements-menu').innerHTML = mapHTML;
+document.querySelector('#elements-menu1').innerHTML = mapHTML1;
+document.querySelector('#elements-menu2').innerHTML = mapHTML2;
 
 function h1(content) {
-	mapHTML += `<h1>${content}</h1>`;
+	var el = `<h1>${content}</h1>`;
+	if (currentHTML == 1) {
+		mapHTML1 += el;
+	} else {
+		mapHTML2 += el;
+	}
 }
 function h2(content) {
-	mapHTML += `<h2>${content}</h2>`;
+	var el = `<h2>${content}</h2>`;
+	if (currentHTML == 1) {
+		mapHTML1 += el;
+	} else {
+		mapHTML2 += el;
+	}
 }
 function div(page, theme, content) {
-	mapHTML += `<div class="element-menu" data-page="${page}" data-theme="${theme}">${content}</div>`;
+	var el = `<div class="element-menu" data-page="${page}" data-theme="${theme}">${content}</div>`;
+	if (currentHTML == 1) {
+		mapHTML1 += el;
+	} else {
+		mapHTML2 += el;
+	}
 }
 
 
