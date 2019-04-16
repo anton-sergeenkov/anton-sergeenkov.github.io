a110c14bbae00695c47c6d80887cdaf5f43010ce
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-04-16 11:50:55 +0300

    del

--- files/webpack 1 (css, image)/src/index.js
+++
@@ -1,7 +0,0 @@
-import { a } from "./module.js";
-document.querySelector('body').innerHTML += '<h2>'+a+'</h2>';
-document.querySelector('body').innerHTML += '<h2>index.js</h2>';
-
-import './index.css';
-// import './pic.jpg';
-
