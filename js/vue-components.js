4b12b59b6aafd91b3229c0d5a714c3455ef598e6
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-06-25 18:56:06 +0300

    vue-components.js

--- js/vue-components.js
+++ js/vue-components.js
@@ -1,28 +1,6 @@
-
-let customElementRegistry = window.customElements;
-let arrCustomElements = [
-	'u-code-text',    'u-code-list',     'u-code-comment', 'u-code',
-	'u-text-define',  'u-text-header',   'u-code-wrapper', 'u-link-wrapper',
-	'u-code-npm',     'u-code-title',    'u-message-ok',   'u-message-info',
-	'u-message-warn', 'u-message-error', 'u-code-accent',  'u-menu',
-	'u-menu-accent',  'u-menu-code',     'u-menu-border',  'u-text-underline',
-	'u-fr1',          'u-fr2',           'u-fr3',
-
-	'bg-methods',     'bg-arguments',
-];
-
-arrCustomElements.forEach(function(element) {
-	customElementRegistry.define(element,
-		class extends HTMLElement {
-			constructor() {
-				super();
-			}
-		}
-	);
-});
-
-
-
+//---------------------------------------------------
+// VUE COMPONENTS
+//---------------------------------------------------
 Vue.component('v-two-code', {
 	props: ['type', 'comment'],
 	computed: {
@@ -102,8 +80,4 @@ Vue.component('v-method-return', {
 			<span class="el arg">return</span>
 			<span class="el text">- <slot></slot></span>
 		</div>`,
-})
-
-var app = new Vue({
-	el: '#content'
 })
