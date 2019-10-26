895ee6e20c7829d286c5ebeb01545c2de25391aa
anton-sergeenkov <warnes23@yandex.ru>
2019-10-26 14:14:13 +0300

    change

--- #notes/module/preloader/preloader.js
+++ #notes/module/preloader/preloader.js
@@ -1,9 +1,11 @@
 
 // убираем скролл при загрузке страницы
-//document.body.style.overflow='hidden';
+document.body.style.overflow='hidden';
 
 // полная загрузка страницы (для jquery 3 версии)
 $(window).on('load', function () {
     $('#preloader').fadeOut(500); // убираем прелоадер
     document.body.style.overflow='visible'; // показываем скролл
 });
+
+
