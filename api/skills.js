f073fe1cd6b3827862d359430fde52f5175f929d
Anton <warnes23@yandex.ru>
2020-06-20 20:14:00 +0300

    Удален Sass, добавлен Vue

---
+++ api/skills.js
@@ -0,0 +1,40 @@
+const skills = [
+	{
+		title: 'Верстка',
+		grid: 'html',
+		skill: ['HTML5', 'CSS3', 'Bootstrap', 'Flexbox', 'CSS Grid', 'SVG', 'CSS Custom Properties', 'Sass', 'PostCSS', 'CSS-in-JS', 'CSS Modules']
+	},
+	{
+		title: 'JavaScript',
+		grid: 'js',
+		skill: ['JavaScript', 'ES5', 'ES6', 'Canvas', 'Ajax', 'Axios', 'jQuery', 'Flow']
+	},
+	{
+		title: 'Vue.js',
+		grid: 'vue',
+		skill: ['Vue.js', 'Vuex', 'Vue Router', 'Vue CLI 3', 'Vuetify.js']
+	},
+	{
+		title: 'React.js',
+		grid: 'react',
+		skill: ['React.js', 'React Hooks', 'Redux', 'React Router', 'PropTypes', 'Formik', 'Material-UI', 'Storybook']
+	},
+	{
+		title: 'Тестирование',
+		grid: 'testing',
+		skill: ['Jest', 'Enzyme', 'Cypress']
+	},
+	{
+		title: 'Технологии',
+		grid: 'tech',
+		skill: ['Webpack', 'Babel', 'Gulp', 'NPM', 'Yarn', 'Git', 'GitHub', 'GitLab', 'Bitbucket', 'Jira']
+	},
+	{
+		title: 'Методологии',
+		grid: 'agile',
+		skill: ['Agile', 'Scrum', 'Gitflow']
+	},
+];
+
+
+
---
+++ js/vue-components.js
@@ -0,0 +1,7 @@
+
+new Vue({
+    el: '#skills .skills__wrapper',
+    data: {
+        techContent: skills
+    }
+});
