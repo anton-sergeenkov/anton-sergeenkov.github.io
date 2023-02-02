96657b777821e26a38fab62baa0560df828953d3
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-02-02 15:55:09 +0400

    z

--- pages/contacts/script.js
+++ pages/contacts/script.js
@@ -1,6 +1,5 @@
-// https://raw.githubusercontent.com/anton-sergeenkov/anton-sergeenkov.github.io/master/%40api/contacts.json
-
-const api = '/@api/contacts.json';
+// const api = '/@api/contacts.json';
+const api = 'https://raw.githubusercontent.com/anton-sergeenkov/anton-sergeenkov.github.io/master/%40api/contacts.json'
 
 new Vue({
     el: '#contacts',
