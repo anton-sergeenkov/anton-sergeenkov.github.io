95e90d7d275fe1a7c345aeca2e128185a0bcd130
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-03-16 15:11:18 +0400

    z

--- vue/starter.js
+++ vue/starter.js
@@ -8,8 +8,6 @@ Let's Code
 › CV               https://docs.google.com/document/d/1bFV29u7COEo9mzoAMLKWbKo87WfZn4VpkRHE3SQ25FA
 */
 
-const PATH_IMG = 'img/';
-
 const ARR_STACK = [
     {
         title: 'Frontend',
@@ -29,11 +27,13 @@ const ARR_STACK = [
     },
 ];
 
+const PATH_IMG_PROJECTS = 'img/projects/';
+
 const ARR_PROJECTS = [
     {
         title: "Courses \"Let's Code\"",
         link: 'https://letscode-dev.github.io/',
-        img: PATH_IMG+'projects/letscode.png',
+        img: PATH_IMG_PROJECTS+'letscode.png',
         description: 'School of programming, courses and mentoring',
         links: [
             { name: 'Website', link: 'https://letscode-dev.github.io/' },
@@ -43,7 +43,7 @@ const ARR_PROJECTS = [
     {
         title: "Blog \"Berserker\"",
         link: 'https://dzen.ru/berserker',
-        img: PATH_IMG+'projects/berserker.png',
+        img: PATH_IMG_PROJECTS+'berserker.png',
         description: 'Blog about Scandinavian and Slavic mythology',
         links: [
             { name: 'Yandex Dzen', link: 'https://dzen.ru/berserker' },
@@ -52,7 +52,7 @@ const ARR_PROJECTS = [
     {
         title: "Habr Publications",
         link: 'https://habr.com/ru/users/anton-sergeenkov/posts/',
-        img: PATH_IMG+'projects/habr.png',
+        img: PATH_IMG_PROJECTS+'habr.png',
         description: 'Publications on website Habr',
         links: [
             { name: 'Habr', link: 'https://habr.com/ru/users/anton-sergeenkov/posts/' },
@@ -61,7 +61,7 @@ const ARR_PROJECTS = [
     {
         title: "Web studio \"Panda\"",
         link: 'https://workshop-panda.github.io/',
-        img: PATH_IMG+'projects/panda.png',
+        img: PATH_IMG_PROJECTS+'panda.png',
         description: 'Website development web studio and Freelance projects',
         links: [
             { name: 'Website', link: 'https://workshop-panda.github.io/' },
@@ -70,44 +70,44 @@ const ARR_PROJECTS = [
     },
 ];
 
-const socialIconsPath = 'img/social-icons/';
+const PATH_ICONS_SOCIAL = 'img/social-icons/';
 
 const ARR_SOCIAL_ICONS = [
     {
-        img: socialIconsPath+'linkedin.png',
+        img: PATH_ICONS_SOCIAL+'linkedin.png',
         title:'Linkedin',
         link: 'https://www.linkedin.com/in/sergeenkov-anton',
         active: true
     },
     {
-        img: socialIconsPath+'github.png',
+        img: PATH_ICONS_SOCIAL+'github.png',
         title:'GitHub',
         link: 'https://github.com/anton-sergeenkov',
         active: true
     },
     {
-        img: socialIconsPath+'telegram.png',
+        img: PATH_ICONS_SOCIAL+'telegram.png',
         title:'Telegram',
         link: 'https://t.me/anton_sergeenkov',
         active: true
     },
     {
-        img: socialIconsPath+'skype.png',
+        img: PATH_ICONS_SOCIAL+'skype.png',
         title:'Skype',
         link: 'https://join.skype.com/invite/hZmBtAZQ0EXY'
     },
     {
-        img: socialIconsPath+'facebook.png',
+        img: PATH_ICONS_SOCIAL+'facebook.png',
         title:'Facebook',
         link: 'https://www.facebook.com/profile.php?id=100002126643722'
     },
     {
-        img: socialIconsPath+'instagram.png',
+        img: PATH_ICONS_SOCIAL+'instagram.png',
         title:'Instagram',
         link: 'https://www.instagram.com/anton.sergeenkov'
     },
     {
-        img: socialIconsPath+'vk.png',
+        img: PATH_ICONS_SOCIAL+'vk.png',
         title:'VKontakte',
         link: 'https://vk.com/anton.sergeenkov'
     },
@@ -116,12 +116,12 @@ const ARR_SOCIAL_ICONS = [
 new Vue({
     el: '#root',
     data: {
-        sectionProjects: null,
+        projects: null,
         socialIcons: null,
         stack: null,
     },
     mounted() {
-        this.sectionProjects = ARR_PROJECTS;
+        this.projects = ARR_PROJECTS;
         this.socialIcons = ARR_SOCIAL_ICONS;
         this.stack = ARR_STACK;
     },
