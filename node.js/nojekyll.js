42f591f0f91a6ad4d99476c2d8208913c2b9b040
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-01-31 20:10:33 +0400

    Bash Script Commit

--- node.js/nojekyll.js
+++ node.js/nojekyll.js
@@ -1,5 +1,11 @@
 const fs = require("fs")
 
+// Директории
+if (!fs.existsSync("./out")) {
+  fs.mkdirSync("./out")
+}
+
+// Файлы
 try {
   fs.writeFileSync("./out/.nojekyll", "")
   console.log('File ".nojekyll" written successfully')
