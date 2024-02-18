e6fa6bc97457bcae344951dd3d9f7611ab700412
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-02-18 15:18:22 +0400

    Bash Script Commit

--- app/qr/components/SectionStarter/SectionStarter.jsx
+++ app/qr/components/SectionStarter/SectionStarter.jsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { useState } from "react"
+
+import Modal from "@mui/material/Modal"
+
 import cn from "classnames"
 
 import { LIST_QR } from "./data/qr"
@@ -7,6 +11,16 @@ import { LIST_QR } from "./data/qr"
 import styles from "./SectionStarter.module.css"
 
 const SectionStarter = () => {
+  const [open, setOpen] = useState(false)
+  const [image, setImage] = useState(null)
+
+  const handleOpen = (img) => {
+    setImage(img)
+    setOpen(true)
+  }
+
+  const handleClose = () => setOpen(false)
+
   return (
     <div className={cn(styles.wrapper)}>
       <div className={cn(styles.container, "section")}>
@@ -14,6 +28,7 @@ const SectionStarter = () => {
           {LIST_QR.map((item, index) => (
             <div className={styles.itemContainer} key={index}>
               <div
+                onClick={() => handleOpen(item.img.src)}
                 className={styles.logo}
                 style={{
                   backgroundImage: `url(${item.img.src})`,
@@ -22,6 +37,21 @@ const SectionStarter = () => {
               <div className={styles.title}>{item.title}</div>
             </div>
           ))}
+
+          <Modal
+            disableAutoFocus
+            open={open}
+            onClose={handleClose}
+            className={styles.modal}
+          >
+            <div
+              onClick={handleClose}
+              className={styles.modalLogo}
+              style={{
+                backgroundImage: `url(${image})`,
+              }}
+            ></div>
+          </Modal>
         </div>
       </div>
     </div>
