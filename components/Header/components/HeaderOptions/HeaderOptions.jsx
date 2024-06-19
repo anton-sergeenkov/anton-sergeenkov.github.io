d2c5aade3382f37a2dcb0581bd2f855988bddb2d
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-06-19 09:14:50 +0400

    Bash Script Commit

--- components/Header/components/HeaderOptions/HeaderOptions.jsx
+++ components/Header/components/HeaderOptions/HeaderOptions.jsx
@@ -1,65 +1,52 @@
 "use client"
 
-import { useState } from "react"
-
-// import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined'
-import Menu from "@mui/material/Menu"
 import MenuItem from "@mui/material/MenuItem"
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
 
 import { ROUTES } from "@/routes/routes"
 import { THEME_MENU } from "@/constants/theme"
 
-import {
-  MenuButtonElement,
-  MenuItemRoutesElement,
-} from "@/sawyer-react-lib/ui-kit"
+import { MenuItemRoutesElement } from "@/sawyer-react-lib/ui-kit"
+
+import MenuToggleElement from "../MenuToggleElement"
 
 import styles from "./HeaderOptions.module.css"
 
-const HeaderOptions = (props) => {
-  const { isAccent } = props
-
-  const [anchorEl, setAnchorEl] = useState(null)
-  const open = Boolean(anchorEl)
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+const HeaderOptionsContent = (props) => {
+  const { list, handleClose } = props
 
   return (
     <>
-      <MenuButtonElement
-        theme={{ mode: THEME_MENU }}
-        isAccent={isAccent}
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        Menu
-      </MenuButtonElement>
-
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        {ROUTES.map((item, index) => (
-          <div className={styles.link} key={index}>
-            <MenuItem
-              onClick={handleClose}
-              sx={{
-                padding: 0,
-                margin: 0,
-                minHeight: "auto",
-                cursor: "default",
-              }}
-            >
-              <MenuItemRoutesElement theme={{ mode: THEME_MENU }} item={item} />
-            </MenuItem>
-          </div>
-        ))}
-      </Menu>
+      {list.map((item, index) => (
+        <div className={styles.link} key={index}>
+          <MenuItem
+            onClick={handleClose}
+            sx={{
+              padding: 0,
+              margin: 0,
+              minHeight: "auto",
+              cursor: "default",
+            }}
+          >
+            <MenuItemRoutesElement theme={{ mode: THEME_MENU }} item={item} />
+          </MenuItem>
+        </div>
+      ))}
     </>
+  )
+}
+
+const HeaderOptions = (props) => {
+  const { className, isAccent } = props
+
+  return (
+    <MenuToggleElement
+      className={className}
+      theme={{ mode: THEME_MENU }}
+      list={ROUTES}
+      title="Menu"
+      isAccent={isAccent}
+      Component={HeaderOptionsContent}
+    />
   )
 }
 
--- components/Header/components/MenuOptions/MenuOptions.jsx
+++ components/Header/components/MenuOptions/MenuOptions.jsx
@@ -1,83 +1,70 @@
 "use client"
 
-import { useState } from "react"
-
 // import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined'
-import Menu from "@mui/material/Menu"
 import MenuItem from "@mui/material/MenuItem"
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
-
 import { ButtonElement } from "@/sawyer-react-lib/ui-kit"
+import MenuToggleElement from "../MenuToggleElement"
 
 import { THEME_MENU } from "@/constants/theme"
 
-import { MenuButtonElement } from "@/sawyer-react-lib/ui-kit"
-
 import styles from "./MenuOptions.module.css"
+
+const MenuOptionsContent = (props) => {
+  const { list, handleClose } = props
+
+  return (
+    <>
+      {list.items.map((itemMenu, indexMenu) => (
+        <MenuItem
+          key={indexMenu}
+          onClick={handleClose}
+          sx={{
+            padding: 0,
+            margin: 0,
+            minHeight: "auto",
+            cursor: "default",
+          }}
+        >
+          <ButtonElement
+            size="small"
+            fullWidth
+            variant="text"
+            disabled={itemMenu.disabled}
+            // endIcon={<OpenInNewOutlinedIcon className={styles.icon} />}
+            sx={{
+              textTransform: "none",
+              color: "#2b2b2b",
+              justifyContent: "start",
+              padding: "5px 10px",
+              fontFamily: "var(--font-roboto-light)",
+            }}
+          >
+            {!itemMenu.disabled ? (
+              <a className={styles.link} href={itemMenu.path} target="_blank">
+                {itemMenu.title}
+              </a>
+            ) : (
+              <span>{itemMenu.title}</span>
+            )}
+          </ButtonElement>
+        </MenuItem>
+      ))}
+    </>
+  )
+}
 
 const MenuOptions = (props) => {
   const { item, isAccent, className } = props
 
-  const [anchorEl, setAnchorEl] = useState(null)
-  const open = Boolean(anchorEl)
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   return (
-    <div className={className}>
-      <MenuButtonElement
-        theme={{ mode: THEME_MENU }}
-        isAccent={isAccent}
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        {item.title}
-      </MenuButtonElement>
-
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-        {item.items.map((itemMenu, indexMenu) => (
-          <MenuItem
-            key={indexMenu}
-            onClick={handleClose}
-            sx={{
-              padding: 0,
-              margin: 0,
-              minHeight: "auto",
-              cursor: "default",
-            }}
-          >
-            <ButtonElement
-              size="small"
-              fullWidth
-              variant="text"
-              disabled={itemMenu.disabled}
-              // endIcon={<OpenInNewOutlinedIcon className={styles.icon} />}
-              sx={{
-                textTransform: "none",
-                color: "#2b2b2b",
-                justifyContent: "start",
-                padding: "5px 10px",
-                fontFamily: "var(--font-roboto-light)",
-              }}
-            >
-              {!itemMenu.disabled ? (
-                <a className={styles.link} href={itemMenu.path} target="_blank">
-                  {itemMenu.title}
-                </a>
-              ) : (
-                <span>{itemMenu.title}</span>
-              )}
-            </ButtonElement>
-          </MenuItem>
-        ))}
-      </Menu>
-    </div>
+    <MenuToggleElement
+      className={className}
+      theme={{ mode: THEME_MENU }}
+      list={item}
+      title={item.title}
+      isAccent={isAccent}
+      Component={MenuOptionsContent}
+    />
   )
 }
 
