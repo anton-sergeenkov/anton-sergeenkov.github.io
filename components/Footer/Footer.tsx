2a6b299dd565563889a3bef0a280830775e933ed
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-11-02 10:09:10 +0400

    Bash Script Commit

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import cn from "classnames"
 import LayoutSectionElement from "@/sawyer-react-lib/ui-kit/layout/LayoutSectionElement"
-import ChipsElement from "@/sawyer-react-lib/ui-kit/basic/ChipsElement"
 import { THEME } from "@/constants/theme"
 
 import styles from "./Footer.module.css"
@@ -15,20 +14,7 @@ const Footer: React.FC = () => {
         <span className={cn(styles.items, styles.logoConteiner)}>
           <div className={styles.logo}></div>Anton Sergeenkov
         </span>
-        <span className={styles.items}>
-          <ChipsElement
-            items={["React"]}
-            className={cn(styles.chips, styles.chip1)}
-            isUserSelect
-          />
-          &
-          <ChipsElement
-            items={["Vue"]}
-            className={cn(styles.chips, styles.chip2)}
-            isUserSelect
-          />
-          Frontend Developer
-        </span>
+        <span className={styles.items}>React & Vue Frontend Developer</span>
       </LayoutSectionElement>
     </div>
   )
