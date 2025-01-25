fea1fe31da814656ca2b29004752e87e71d4cb9d
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2025-01-25 16:01:12 +0400

    ok

--- app/uikit/page.tsx
+++ app/uikit/page.tsx
@@ -1,11 +1,13 @@
 import ButtonElements from "./components/ButtonElements"
 import LinkElements from "./components/LinkElements"
+import ChipsElements from "./components/ChipsElements"
 
 const AppPage: React.FC = () => {
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px", background: "#fff" }}>
       <ButtonElements />
       <LinkElements />
+      <ChipsElements />
     </div>
   )
 }
