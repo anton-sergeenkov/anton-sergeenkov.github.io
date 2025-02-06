90d8a6987e785bd6ab14ecabf15e5135c66821ad
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2025-02-06 18:09:32 +0400

    Bash Script Commit

--- app/uikit/page.tsx
+++ app/uikit/page.tsx
@@ -1,6 +1,5 @@
 import Wrapper from "./components/Wrapper"
 
-import UiButtonComponent from "./elements/UiButtonComponent"
 import UiLinkComponent from "./elements/UiLinkComponent"
 import UiChipsComponent from "./elements/UiChipsComponent"
 import UiLayoutCardsComponent from "./elements/UiLayoutCardsComponent"
@@ -8,10 +7,6 @@ import UiLayoutCardsComponent from "./elements/UiLayoutCardsComponent"
 const AppPage: React.FC = () => {
   return (
     <div style={{ padding: "10px 30px", background: "#fff" }}>
-      <Wrapper title="Button">
-        <UiButtonComponent />
-      </Wrapper>
-
       <Wrapper title="Link">
         <UiLinkComponent />
       </Wrapper>
