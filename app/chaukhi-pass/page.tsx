b02478d758e7fdfff2fc2caccc0fdad38a440b5d
Anton Sergeenkov <asergeenkov@combotech.net>
2026-06-18 01:47:27 +0400

    Bash Script Commit

---
+++ app/chaukhi-pass/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+import img1 from "./img/1.png"
+import img2 from "./img/2.png"
+import img3 from "./img/3.png"
+import img4 from "./img/4.png"
+import img5 from "./img/5.png"
+import img6 from "./img/6.png"
+import img7 from "./img/7.png"
+import img8 from "./img/8.png"
+import img9 from "./img/9.png"
+import img10 from "./img/10.png"
+import img11 from "./img/11.png"
+import img12 from "./img/12.png"
+
+import styles from "./styles.module.css"
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+]
+
+const AppPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      {images.map((src, index) => (
+        <Image
+          key={index}
+          className={styles.img}
+          src={src}
+          alt={`Chaukhi Pass ${index + 1}`}
+          width={700}
+          height={1000}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default AppPage
